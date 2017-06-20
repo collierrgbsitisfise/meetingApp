1f0529d739d3a49439c2e7eab42093bbd4a28988
vadim <vadim.vicolaev.v@gmail.com>
2017-06-20 10:19:49 +0300

    test commit

--- server/configs/mailer.config.js
+++ server/configs/mailer.config.js
@@ -1,8 +1,8 @@
 //in {user} and {pass} enter you'r data
 
 const MailerCongif = {
-    user: 'email',
-    pass: 'password'
+    user: 'email23',
+    pass: 'password23'
 }
 
 module.exports.MailerCongif = MailerCongif;
